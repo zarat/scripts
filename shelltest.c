eda5f3ae2a8e87a824c98c2e6869c38ed2420224
zarat <manuel.zarat@gmail.com>
2018-08-28 01:44:13 +0200

    Update shelltest.c

--- shelltest.c
+++ shelltest.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<string.h>
-char *shellcode = "";
-
-int main() {
-  fprintf();
-  (*(void(*)())  shellcode)();
-  return 0;
+#include <stdio.h>
+#include <string.h>
+/* compiling: gcc -fo-stack-protector -z eecstack ile.c -o file -m32 */
+char *shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+int main(void) {
+	fprintf(stdout,"Length: %d\n",strlen(shellcode));
+	(*(void(*)()) shellcode)();
+	return 0;
 }
